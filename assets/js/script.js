5918be57b98a88061476c2630a61381702723218
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-06-22 07:48:20 -0600

    changes

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+        $(document).ready(function() {
+            AOS.init({
+  duration: 1200,
+})
+            $(".pagetransition").click(function() {
+
+                var href = $(this).attr('href');
+
+                // Delay settings
+                setTimeout(function() {window.location = href}, 600);
+                return false;
+            });
+        
+        });
+        
+
+      
