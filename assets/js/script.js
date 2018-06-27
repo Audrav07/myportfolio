c45295b7c5508124a4c07e1d79114f53e82abe91
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-06-27 15:22:31 -0600

    changes to files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
-
-
-
-
-
-        $(document).ready(function() {
-            AOS.init({
-  duration: 1200,
-})
-            $(".pagetransition").click(function() {
-
-                var href = $(this).attr('href');
-
-                // Delay settings
-                setTimeout(function() {window.location = href}, 600);
-                return false;
-            });
-        
-        });
+$(document).ready(function() {
+    AOS.init({
+        duration: 1200,
+    })
+    $(".pagetransition").click(function() {
+        var href = $(this).attr('href');
+        // Delay settings
+        setTimeout(function() {
+            window.location = href
+        }, 600);
+        return false;
+    });
+});
         
 
       
