aea6bb61f02bc11246bb61953bfc3b75d3e20f2f
Audrey <30810025+Audrav07@users.noreply.github.com>
2018-07-10 06:32:19 -0600

    changes added to navbar and clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,20 @@
-$(document).ready(function() {
-    AOS.init({
+    $(document).ready(function() {
+      AOS.init({
         duration: 1200,
-    })
-    $(".pagetransition").click(function() {
-        var href = $(this).attr('href');
-        // Delay settings
-        setTimeout(function() {
-            window.location = href
-        }, 600);
-        return false;
-    });
-});
-        
-$(document).ready(function(){ 
-    $(window).scroll(function(){ 
-        if ($(this).scrollTop() > 100) { 
-            $('#home-top').fadeIn(); 
-        } else { 
-            $('#home-top').fadeOut(); 
-        } 
-    }); 
-    $('#home-top').click(function(){ 
-        $("html, body").animate({ scrollTop: 0 }, 600); 
-        return false; 
-    }); 
-});
+      });
+      $(function() {
+        $('.menu-icon').on('click', function() {
+          toggleMenu();
+          console.log('open');
+        });
+        $('.nav__list-item').on('click', function() {
+          toggleMenu();
+        });
 
-      
+        function toggleMenu() {
+          var menuHandle = $('.menu-icon');
+          menuHandle.toggleClass('active');
+          menuHandle.next().stop().toggleClass('active');
+        }
+      });
+    });
